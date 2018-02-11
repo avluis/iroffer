3ec79710b047ade0f382f261b870d13cc1babcc8
picodexter <picodexter10@gmail.com>
2018-02-11 15:35:04 +0100

    Only set permissions and ownership of directories when creating them because they did not exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,24 +9,30 @@ fi
 
 init_config() {
 # Config
-  mkdir -p ${IROFFER_CONFIG_DIR}
-  if [ ! -e "${IROFFER_CONFIG_DIR}/${IROFFER_CONFIG_FILE_NAME}" ]; then
-    cp /extras/sample.customized.config "${IROFFER_CONFIG_DIR}/${IROFFER_CONFIG_FILE_NAME}"
-    echo "Copied fresh sample configuration to ${IROFFER_CONFIG_DIR}/${IROFFER_CONFIG_FILE_NAME}. Exiting."
-    exit
+  if [ ! -d "${IROFFER_CONFIG_DIR}" ]; then
+    mkdir -p ${IROFFER_CONFIG_DIR}
+    if [ ! -e "${IROFFER_CONFIG_DIR}/${IROFFER_CONFIG_FILE_NAME}" ]; then
+      cp /extras/sample.customized.config "${IROFFER_CONFIG_DIR}/${IROFFER_CONFIG_FILE_NAME}"
+      echo "Copied fresh sample configuration to ${IROFFER_CONFIG_DIR}/${IROFFER_CONFIG_FILE_NAME}. Exiting."
+      exit
+    fi
+    chmod -R 0755 ${IROFFER_CONFIG_DIR}
+    chown -R ${IROFFER_USER}: ${IROFFER_CONFIG_DIR}
   fi
-  chmod -R 0755 ${IROFFER_CONFIG_DIR}
-  chown -R ${IROFFER_USER}: ${IROFFER_CONFIG_DIR}
 
 # Data
-  mkdir -p ${IROFFER_DATA_DIR}
-  chmod -R 0750 ${IROFFER_DATA_DIR}
-  chown -R ${IROFFER_USER}: ${IROFFER_DATA_DIR}
+  if [ ! -d "${IROFFER_DATA_DIR}" ]; then
+    mkdir -p ${IROFFER_DATA_DIR}
+    chmod -R 0750 ${IROFFER_DATA_DIR}
+    chown -R ${IROFFER_USER}: ${IROFFER_DATA_DIR}
+  fi
 
 # Logs
-  mkdir -p ${IROFFER_LOG_DIR}
-  chmod -R 0755 ${IROFFER_LOG_DIR}
-  chown -R ${IROFFER_USER}: ${IROFFER_LOG_DIR}
+  if [ ! -d "${IROFFER_LOG_DIR}" ]; then
+    mkdir -p ${IROFFER_LOG_DIR}
+    chmod -R 0755 ${IROFFER_LOG_DIR}
+    chown -R ${IROFFER_USER}: ${IROFFER_LOG_DIR}
+  fi
 }
 
 # Startup
